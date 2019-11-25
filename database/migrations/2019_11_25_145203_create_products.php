5e170bdbba49866941d71a4cdba0bd9b3ea3e5df
beebones <gelbundschwarz@gmail.com>
2019-11-25 13:05:33 -0200

    Gerando tabelas user, products

---
+++ database/migrations/2019_11_25_145203_create_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',300);
+            $table->string('description', 300);
+            $table->integer('quantity');
+            $table->float('price', 10, 2);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
